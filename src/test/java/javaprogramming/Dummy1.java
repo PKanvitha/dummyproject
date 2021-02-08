2196cefe1b444b6f9ba9bcca550e7d776fac4208
acer <acer@DESKTOP-LO49D0F>
2021-02-08 11:27:26 +0530

    1st commit

---
+++ src/test/java/javaprogramming/Dummy1.java
@@ -0,0 +1,17 @@
+package javaprogramming;
+
+public class Dummy1
+{
+
+	public static void main(String[] args) 
+	{
+		String s="Ram";
+		char[] ch=s.toCharArray();
+		for(int i=0;i<ch.length;i++)
+		{  
+	    System.out.print(ch[i]);  
+		}
+
+	}
+
+}
